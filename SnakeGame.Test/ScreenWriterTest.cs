7fca46f619bef0fa09f66d5fb878ed9cda8f2777
mmiller0482 <75590602+mmiller0482@users.noreply.github.com>
2024-12-13 16:01:27 -0500

    Test for screenwriter

---
+++ SnakeGame.Test/ScreenWriterTest.cs
@@ -0,0 +1,36 @@
+namespace SnakeGame.Test;
+
+public class ScreenWriterTest
+{
+
+    [Fact]
+    public void TestWriteLine_String()
+    {
+        var oldOut = Console.Out;
+        StringWriter capturedOut = new StringWriter();
+        Console.SetOut(capturedOut);
+
+        string sample = "abc";
+        ScreenWriter.WriteLine(sample);
+        
+        Assert.Contains("abc", capturedOut.ToString());
+        
+        Console.SetOut(oldOut);
+    }
+    
+    [Fact]
+    public void TestWriteLine_CharArray()
+    {
+        var oldOut = Console.Out;
+        StringWriter capturedOut = new StringWriter();
+        Console.SetOut(capturedOut);
+
+        char[] sample = ['a', 'b', 'c'];
+        ScreenWriter.WriteLine(sample);
+        
+        Assert.Contains("abc", capturedOut.ToString());
+        
+        Console.SetOut(oldOut);
+    }
+
+}
