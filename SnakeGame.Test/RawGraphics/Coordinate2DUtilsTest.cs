bb94d0f3b144feadb8a5e71d071b9e0a7092d7bf
mmiller0482 <75590602+mmiller0482@users.noreply.github.com>
2024-12-14 07:16:20 -0500

    Added Utility to Wrap Coordinates around a fixed 2D grid.

---
+++ SnakeGame.Test/RawGraphics/Coordinate2DUtilsTest.cs
@@ -0,0 +1,34 @@
+using SnakeGame.RawGraphics;
+
+namespace SnakeGame.Test.RawGraphics;
+
+public class Coordinate2DUtilsTest
+{
+    [Theory]
+    [InlineData(0,0,0,0)]
+    [InlineData(-1,0,1,0)]
+    [InlineData(0,-1,0,1)]
+    public void TestWrapCoordinate_2By2(int origX, int origY, int expectX, int expectY)
+    {
+        Coordinate2D origin = new(origX, origY);
+        Coordinate2D expect = new(expectX, expectY);
+
+        Coordinate2D res = Coordinate2DUtils.WrapCoordinate(origin, 2, 2);
+        
+        Assert.Equal(expect, res);
+    }
+    [Theory]
+    [InlineData(-4, 2, 0)]
+    [InlineData(-3, 2, 1)]
+    [InlineData(-2, 2, 0)]
+    [InlineData(-1, 2, 1)]
+    [InlineData(0, 2, 0)]
+    [InlineData(1, 2, 1)]
+    [InlineData(2, 2, 0)]
+    [InlineData(3, 2, 1)]
+    [InlineData(4, 2, 0)]
+    public void TestMyModulo(int left, int right, int expectedResult)
+    {
+        Assert.Equal(expectedResult, Coordinate2DUtils.ComputeWrappedInteger(left, right));
+    }
+}
