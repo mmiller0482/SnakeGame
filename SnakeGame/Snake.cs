fec324954c67f4ca73ce65dcb1c9e4fb313d11c2
mmiller0482 <75590602+mmiller0482@users.noreply.github.com>
2024-12-15 09:08:32 -0500

    Snake that moves

--- SnakeGame/Snake.cs
+++ SnakeGame/Snake.cs
@@ -1,31 +1,102 @@
+using System.Runtime.InteropServices.JavaScript;
+using SnakeGame.RawGraphics;
+
 namespace SnakeGame;
 
 public class Snake
 {
-    private List<Coordinate2D> _body;
+    private readonly Coordinate2D _startPosition; 
+    private const char SnakeSegmentSymbol = '#';
+    
+    private readonly LinkedList<Coordinate2D> _body = [];
+
+    private bool _shouldEat = false;
+    
 
     public Snake(Coordinate2D startPosition)
     {
-        _body = [startPosition];
+        _body.AddFirst(startPosition);
+        _startPosition = startPosition;
     }
 
-    public void PointAndMoveUp()
+    public void EatFood()
     {
-        
+        _shouldEat = true;
+    }
+    
+    // TODO: Probably need some better behavior when a snake has empty body.
+    public Coordinate2D Head => _body.FirstOrDefault(new Coordinate2D(0, 0));
+
+    // TODO: Probably need some better behavior when a snake has empty body.
+    public Coordinate2D Tail => _body.LastOrDefault(new Coordinate2D(0, 0));
+
+    public void Move(UserDirection userDirection)
+    {
+        switch (userDirection)
+        {
+            case UserDirection.Up:
+                PointAndMoveUp();
+                break;
+            case UserDirection.Down:
+                PointAndMoveDown();
+                break;
+            case UserDirection.Left:
+                PointAndMoveLeft();
+                break;
+            case UserDirection.Right:
+                PointAndMoveRight();
+                break;
+        }
+    }
+    public void PointAndMoveUp() => SnakeTranslate(0, 1);
+
+    public void PointAndMoveDown() => SnakeTranslate(0, -1);
+
+    public void PointAndMoveLeft() => SnakeTranslate(-1, 0);
+
+    public void PointAndMoveRight() => SnakeTranslate(1, 0);
+
+    public void SendToGameBoard(GameBoard gameBoard)
+    {
+        foreach (Coordinate2D segment in _body)
+        {
+            gameBoard.AddSprite(new Sprite(segment, SnakeSegmentSymbol));
+        }
     }
 
-    public void PointAndMoveDown()
+    public void Reset()
     {
-        
+        _shouldEat = false;
+        _body.Clear();
+        _body.AddFirst(_startPosition);
     }
 
-    public void PointAndMoveLeft()
-    {
-        
-    }
+    //public void DetectCollision()
+    //{
+    //    var poppedStart = _body.First;
+    //    _body.RemoveFirst();
+    //    if (_body.Any(c => c == poppedStart!))
+    //    {
+    //        Reset();
+    //    }
+    //    else
+    //    {
+    //        _body.AddFirst(poppedStart);
+    //    }
+    //    
+    //}
 
-    public void PointAndMoveRight()
+    private void SnakeTranslate(int xMove, int yMove)
     {
-        
+        Coordinate2D newHead = new Coordinate2D(Head.X + xMove, Head.Y + yMove);
+        _body.AddFirst(newHead);
+        if (_shouldEat)
+        {
+            _shouldEat = false;
+        }
+        else
+        {
+            _body.RemoveLast();
+        }
     }
 }
--- SnakeGame/SnakeGameInstance.cs
+++ SnakeGame/SnakeGameInstance.cs
@@ -1,30 +1,74 @@
+using System.Drawing;
+using SnakeGame.RawGraphics;
+
 namespace SnakeGame;
 
 public class SnakeGameInstance
 {
     private Grid _grid;
+    private GameBoard _gameBoard;
     private Snake _snake;
+    private UserDirection _direction = UserDirection.Right;
+    private Coordinate2D _sentinelCoordinate = new Coordinate2D(1, 1);
 
     public SnakeGameInstance()
     {
-        _grid = new Grid(20, 80, ' ');
-        _snake = new Snake(new Coordinate2D(5, 30));
+        _grid = new Grid(100, 30, ' ');
+        _gameBoard = new GameBoard(_grid, true);
+        _snake = new Snake(_sentinelCoordinate);
     }
-
     public void Run()
     {
         int cycles = 0;
-        while (cycles < 100)
+        while (true)
         {
             Draw();
-            
-            
+            SimulateWait();
+            GetUserDirection();
+            // Operations for this cycle
+            if (cycles % 10 == 0)
+            {
+                _snake.EatFood();
+            }
+            _snake.Move(_direction);
+            _gameBoard.Clear();
             cycles++;
         }
     }
+
+    private void SimulateWait()
+    {
+            System.Threading.Thread.Sleep(50); // Control speed of snake movement
+    }
     private void Draw()
     {
-        
+        _snake.SendToGameBoard(_gameBoard); 
+        _gameBoard.Render();
+        GridWriter.Draw(_grid);
+         
+    }
+
+    private void GetUserDirection()
+    {
+        if (Console.KeyAvailable)
+        {
+            var keyInfo = Console.ReadKey(intercept: true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    _direction = UserDirection.Up;
+                    break;
+                case ConsoleKey.DownArrow:
+                    _direction = UserDirection.Down;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    _direction = UserDirection.Left;
+                    break;
+                case ConsoleKey.RightArrow:
+                    _direction = UserDirection.Right;
+                    break;
+            }
+        } 
     }
     
     
