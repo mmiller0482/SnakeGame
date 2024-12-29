58019acb572d172b4b4ea00c7aa80f16efd5c97d
matthewmiller <mmiller0482@gmail.com>
2024-12-29 10:30:00 -0500

    Significantly improved readability of WriteNormalLine in ScreenWriter.cs. Added vertical borders to the edges of the lines.

--- SnakeGame/RawGraphics/ScreenWriter.cs
+++ SnakeGame/RawGraphics/ScreenWriter.cs
@@ -12,16 +12,16 @@ public static class ScreenWriter
         Console.WriteLine(line);
     }
 
-
     public static void WriteNormalLine(char[] line)
     {
-        char[] normalLine = new char[line.Length*2];
-        for (int i = 0; i < line.Length; i++)
-        {
-            normalLine[i*2] = line[i];
-            normalLine[i*2+1] = ' ';
-        }
-        Console.WriteLine(normalLine);
+        // Create the normalized content with spaces
+        var normalizedContent = string.Join(" ", line);
+
+        // Add borders to the normalized content
+        var borderedLine = $"|{normalizedContent}|";
+
+        // Write the bordered line to the console
+        Console.WriteLine(borderedLine);
     }
     public static void Clear()
     {
