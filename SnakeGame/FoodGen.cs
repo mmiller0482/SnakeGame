4a97fa55bf51f66eb2f964a4cb8248355b6d411a
matthewmiller <mmiller0482@gmail.com>
2024-12-29 10:17:26 -0500

    Forgot to check in FoodGen

---
+++ SnakeGame/FoodGen.cs
@@ -0,0 +1,24 @@
+
+using SnakeGame.RawGraphics;
+
+namespace SnakeGame;
+
+public class FoodGen
+{
+    private const char DefaultFoodSymbol = 'X';
+    private readonly char _foodSymbol;
+    private readonly Coordinate2DFactory _coordinate2DFactory;
+
+
+    public FoodGen(Coordinate2DFactory coordinateFactory, char foodSymbol = DefaultFoodSymbol)
+    {
+        _coordinate2DFactory = coordinateFactory;
+        _foodSymbol = foodSymbol;
+    }
+
+    public Sprite RandomFood() => RandomFood([]);
+    
+    public Sprite RandomFood(IEnumerable<Coordinate2D> lockoutCoordinates) 
+        => new Sprite(_coordinate2DFactory.CreateRandom(lockoutCoordinates), _foodSymbol);
+    
+}
